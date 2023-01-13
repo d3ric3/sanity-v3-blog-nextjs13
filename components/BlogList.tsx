cb632f0438200fe7f26766df26ef81434df257c7
d3ric3 <d3ric3@gmail.com>
2023-01-13 12:23:59 +0800

    progress 1:32:40

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,9 +1,31 @@
+import Image from "next/image";
+import urlFor from "../lib/urlFor";
+
 type Props = {
   posts: Post[];
 };
 
 function BlogList({ posts }: Props) {
-  return <div>BlogList</div>;
+  return (
+    <div>
+      <hr className="border-[#F7AB0A] mb-10" />
+
+      <div>
+        {posts.map((post) => (
+          <div key={post._id}>
+            <div>
+              <Image
+                className="object-cover object-left lg:object-center"
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default BlogList;
